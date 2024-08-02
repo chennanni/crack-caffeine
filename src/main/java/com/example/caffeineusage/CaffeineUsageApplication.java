03fa4b146995840de669bb46e13b20d49fe00eef
Chennan <chennanni@users.noreply.github.com>
2024-08-02 10:04:31 +0800

    init commit

---
+++ src/main/java/com/example/caffeineusage/CaffeineUsageApplication.java
@@ -0,0 +1,13 @@
+package com.example.caffeineusage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaffeineUsageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaffeineUsageApplication.class, args);
+    }
+
+}
